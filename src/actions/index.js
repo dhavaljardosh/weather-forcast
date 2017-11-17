338775c90c6ce7ba135a9f5e692e6844cac086ca
Dhaval Jardosh <dhavaljardosh@gmail.com>
2017-11-17 17:08:56 -0600

    Fetching data for weather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
 const API_KEY = '3e18e703d0466a03578f6022a2bc5fbe';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
   const request = axios.get(url);
+
+  console.log('Request: ', request);
 
   return {
     type: FETCH_WEATHER,
